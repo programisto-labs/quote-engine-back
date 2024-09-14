33a89e0f07466bb0c221b1ce41fcabf2ce49523f
aramisstalin <aramis.stalin@gmail.com>
2024-09-14 16:45:15 -0300

    Fixing test builds

--- src/test/java/com/programisto/devis_rapide/DevisRapideApplicationTests.java
+++ src/test/java/com/programisto/devis_rapide/DevisRapideApplicationTests.java
@@ -3,10 +3,8 @@ package com.programisto.devis_rapide;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
-@ComponentScan(basePackages = {"com.programisto.devis_rapide"})
 class DevisRapideApplicationTests {
 
 	@Test
