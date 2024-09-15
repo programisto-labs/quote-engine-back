c9f2a64e621c2ae33b83d61fa0d6611651154da6
aramisstalin <aramis.stalin@gmail.com>
2024-09-14 22:43:28 -0300

    Adding async config to send emails

--- src/main/java/com/programisto/devis_rapide/DevisRapideApplication.java
+++ src/main/java/com/programisto/devis_rapide/DevisRapideApplication.java
@@ -2,8 +2,10 @@ package com.programisto.devis_rapide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class DevisRapideApplication {
 
 	public static void main(String[] args) {
